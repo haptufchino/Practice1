766fa8ba968b5603bc3dda598be79339bccfddfe
Artur <arturkaz333@gmail.com>
2026-02-06 11:52:01 +0500

    initial commit

---
+++ whileloops.py
@@ -0,0 +1,25 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1 #print i while i is less than 6
+  	
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1 #exit the loop when i is 3
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i) #continue to the next iteration if i is 3
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6") #leave the message once condition is false
